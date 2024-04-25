8f194e1bc2fccba1bc1584893d3f04f1946916b5
logumano1 <danimartinromo@gmail.com>
2024-04-25 16:15:27 +0200

    edcec

---
+++ CABRA/src/components/SingleFile.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+const saludo = ref("hola a todos")
+
+</script>
+
+<template>
+    <div>
+
+        {{ saludo }}
+
+    </div>
+</template>
